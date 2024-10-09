9cc8c196d4c7d0c841ed0e37c4dc852a6610ead4
mohamed-farouk07 <60257984+mohamed-farouk07@users.noreply.github.com>
2024-10-09 05:44:53 +0200

    setup

---
+++ todo-list/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+}
+</style>
---
+++ todo-list/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h2>To-Do List</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #fff;
+  color: #3c495a;
+  padding: 10px;
+  margin: 0;
+  border-bottom: 3px solid #3c495a;
+}
+</style>
